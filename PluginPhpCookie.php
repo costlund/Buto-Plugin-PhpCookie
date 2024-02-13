94c8dd6f0f45f723db13b6fccfb4aa8b500f0477
Claes Östlund <claes@stenbergit.se>
2024-02-13 15:39:52 +0100

    1.3.0

--- PluginPhpCookie.php
+++ PluginPhpCookie.php
@@ -5,6 +5,7 @@ class PluginPhpCookie{
   public $domain = "";
   public $secure = true;
   public $httponly = false;
+  public $samesite = 'Lax'; //The value of the samesite element should be either None, Lax or Strict.
   public function set($name, $value){
     /**
      * secure
@@ -15,11 +16,29 @@ class PluginPhpCookie{
     /**
      * setcookie
      */
-    setcookie($name, $value, strtotime( '+'.$this->days.' days' ), $this->path, $this->domain, $this->secure, $this->httponly);
+    if(wfGlobals::get('php/version')<='7.3'){
+      setcookie($name, $value, strtotime( '+'.$this->days.' days' ), $this->path, $this->domain, $this->secure, $this->httponly);
+    }else{
+      $options = new PluginWfArray();
+      $options->set('expires' , strtotime( '+'.$this->days.' days' ));
+      $options->set('path'    , $this->path);
+      $options->set('domain'  , $this->domain);
+      $options->set('secure'  , $this->secure);
+      $options->set('httponly', $this->httponly);
+      $options->set('samesite', $this->samesite);
+      setcookie($name, $value, $options->get());
+    }
     return null;
   }
   public function del($name){
-    setcookie($name, '', time()-1000, "/");
+    if(wfGlobals::get('php/version')<='7.3'){
+      setcookie($name, '', time()-1000, $this->path);
+    }else{
+      $options = new PluginWfArray();
+      $options->set('expires' , time()-1000);
+      $options->set('path'    , $this->path);
+      setcookie($name, '', $options->get());
+    }
   }
   public function get($name){
     if(isset($_COOKIE[$name])){
